f968a0c19f341e985a42e96842603372347dfe50
shaokai.wang <shaokai.wang@terapines.com>
2025-03-06 14:31:37 +0800

    [003]支持空格--- rvcc  ' 12 - 34 + 56' = 34

--- main.c
+++ main.c
@@ -1,5 +1,130 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+// 为每个终结符设置种类
+typedef enum 
+{
+    TK_PUNCT, // 操作符
+    TK_NUM,   // 数字
+    TK_EOF,   // 文件结束符
+} TokenKind;
+
+// 终结符结构体
+typedef struct Token Token;
+struct Token
+{
+    TokenKind kind;   // 终结符种类
+    Token *Next;      // 下一个Token
+    int Val;          // 终结符的值
+    char *Loc;        // 终结符的位置
+    int Len;          // 终结符的长度
+};
+
+/// @brief 输出错误信息
+/// static表示该函数为静态函数，只在当前文件可见
+/// @param Fmt 传入字符串
+/// @param ... 可变参数，表示Fmt后面死哦用参数
+static void error(char *Fmt, ...) 
+{
+    // 定一个va_list变量
+    va_list VA;
+    // VA获取Fmt后面的所有参数
+    va_start(VA, Fmt);
+    // vfprintf可以输出va_list类型参数
+    vfprintf(stderr, Fmt, VA);
+    // 结束va_start
+    va_end(VA);
+    exit(1);
+}
+
+// 生成新的Token
+static Token *new_token(TokenKind King, char *Start, char *End)
+{
+    // 分配1个Token的内存空间
+    Token *Tok = calloc(1, sizeof(Token));
+    Tok->kind = King;
+    Tok->Loc = Start;
+    Tok->Len = End - Start;
+    return Tok;
+}
+
+// 终结符解析
+static Token *tokenize(char *P)
+{
+    Token Head = {};
+    Token *Cur = &Head;
+
+    while(*P)
+    {
+        // 跳过所有空白、回车
+        if(isspace(*P))
+        {
+            ++P;
+            continue;
+        }
+
+        // 解析数字
+        if(isdigit(*P))
+        {
+           // 初始化 Token结构体
+           // 不使用Head来存储信息，仅用来表示链表入口，这样每次都是存储在Cur->Next中
+           // 否则下述操作将使第一个Token的地址不在Head中
+           Cur->Next = new_token(TK_NUM, P, P);
+           // 指针前进
+           Cur = Cur->Next;
+           const char *oldP = P;
+           Cur->Val = strtoul(P, &P, 10);
+           Cur->Len = P - oldP;
+           continue;
+        }
+
+        // 解析操作符
+        if(*P == '+' || *P == '-')
+        {
+            // 操作符长度为1
+            Cur->Next = new_token(TK_PUNCT, P, P + 1);
+            Cur = Cur->Next;
+            ++P;
+            continue;
+        }
+
+        // 无法处理的字符
+        error("invalid character: '%c'\n", *P);
+    }
+    // 解析结束，增加一个EOF,表示终止符
+    Cur->Next = new_token(TK_EOF, P, P);
+    // 返回头结点
+    return Head.Next;
+}
+
+// 返回TK_NUM的值
+static int getNumber(Token *Tok)
+{
+    if(Tok->kind != TK_NUM)
+        error("not a number");
+    return Tok->Val;
+}
+
+// 判断Tok的值是否等与指定的值
+static bool equal(Token *Tok, char *Str)
+{
+    // 比较字符串LHS(左)，RHS(右)的前N位，S2的长度应大于N
+    // 比较按照字典序，LHS<RHS回负值，LHS=RHS返回0，LHS>RHS返回正值
+    // 同时确保，此处的Op位数=N
+    return memcmp(Tok->Loc, Str, Tok->Len) == 0 && Str[Tok->Len] == '\0';
+}
+
+// 跳过指定的str
+static Token *skip(Token *Tok, char *Str)
+{
+    if (!equal(Tok, Str))
+        error("expect '%s' ",Str);
+    return Tok->Next;
+}
 
 int main(int argc,char ** argv)
 {
@@ -8,48 +133,38 @@ int main(int argc,char ** argv)
         // 异常处理，提示参数数量不对
         // fprintf,格式化文件输出，往文件内写入字符串
         // stderr,异常文件, 用于往屏幕显示异常信息
-        fprintf(stderr,"%s: invalid number of arguments\n", argv[0]);
-        return 1;    
+       error("%s: invalid number of arguments",argv[0]);
     }
-    // P保存输入的算式的str
-    // 不声明const是为了后续的递增操作
-    char *P = argv[1];
+
+    // 解析Argv[1]
+    Token *Tok = tokenize(argv[1]);
 
     // 声明全局mian段，同时也是程序的入口
     printf(".globl main\n");
     // main段标签
     printf("main:\n");
 
-    // li为addi别名指令，加载一个立即数到寄存器中
-    // 传入程序的参数位str类型，因为需要转换为long类型
-    // strtol为“string to long”
-    // 参数为：被转换的str，str除去数字后的剩余部分，进制
-    // 传入&P，即char**,是为了修改p的值
+
     // 这里我们将算式分解为 num op num num ... 的形式
-    printf("\tli a0, %ld\n", strtol(P, &P, 10));
-    // 解析：$a0为寄存器，strtol将p转换为long类型，并赋值给$a0
-    while(*P)
+    printf("\tli a0, %d\n", getNumber(Tok));
+    Tok = Tok->Next;
+
+    // 解析 （op num）
+    while(Tok->kind != TK_EOF)
     {
-        // 解析op
-        if(*P == '+')
+        if(equal(Tok, "+"))
         {
-            ++P;
-            // 解析数字
-            // addi rd, rs1, imm 表示rd = rs1 + imm
-            printf("\taddi a0, a0 , %ld\n", strtol(P, &P, 10));
+            Tok = Tok->Next;
+            printf("\tadd a0, a0, %d\n", getNumber(Tok));
+            Tok = Tok->Next;
             continue;
         }
-        if(*P == '-')
-        {
-            ++P;
-            // 解析数字
-            // addi中的imm为有符号数 表示rd = rs1 + （-imm）
-            printf("\taddi a0, a0 , -%ld\n", strtol(P, &P, 10));
-            continue;
-        }
-        // 如果存在未解析的字符，则报错
-        fprintf(stderr, "unexpected character: '%c'\n", *P);
-        return 1;
+
+        // 不是 + 则 判断为 - 
+        // 没有subi 但是addi支持有符号数，所以直接对num取反
+        Tok = skip(Tok, "-");
+        printf("\taddi a0, a0, -%d\n", getNumber(Tok));
+        Tok = Tok->Next;
     }  
 
     // ret为 <jalr x0, x1, 0>别名指令， 用于返回子程序
--- temp/temp.c
+++ temp/temp.c
@@ -3,3 +3,61 @@ void test_001()
     // 传入程序参数为str类型,因为需要转换为int类型使用atoi
     // printf("\tli a0, %d\n",atoi(argv[1]));
 }
+
+int test_002(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        // 异常处理，提示参数数量不对
+        // fprintf,格式化文件输出，往文件内写入字符串
+        // stderr,异常文件, 用于往屏幕显示异常信息
+        fprintf(stderr,"%s: invalid number of arguments\n", argv[0]);
+        return 1;    
+    }
+    // P保存输入的算式的str
+    // 不声明const是为了后续的递增操作
+    char *P = argv[1];
+
+    // 声明全局mian段，同时也是程序的入口
+    printf(".globl main\n");
+    // main段标签
+    printf("main:\n");
+
+    // li为addi别名指令，加载一个立即数到寄存器中
+    // 传入程序的参数位str类型，因为需要转换为long类型
+    // strtol为“string to long”
+    // 参数为：被转换的str，str除去数字后的剩余部分，进制
+    // 传入&P，即char**,是为了修改p的值
+    // 这里我们将算式分解为 num op num num ... 的形式
+    printf("\tli a0, %ld\n", strtol(P, &P, 10));
+    // 解析：$a0为寄存器，strtol将p转换为long类型，并赋值给$a0
+    while(*P)
+    {
+        // 解析op
+        if(*P == '+')
+        {
+            ++P;
+            // 解析数字
+            // addi rd, rs1, imm 表示rd = rs1 + imm
+            printf("\taddi a0, a0 , %ld\n", strtol(P, &P, 10));
+            continue;
+        }
+        if(*P == '-')
+        {
+            ++P;
+            // 解析数字
+            // addi中的imm为有符号数 表示rd = rs1 + （-imm）
+            printf("\taddi a0, a0 , -%ld\n", strtol(P, &P, 10));
+            continue;
+        }
+        // 如果存在未解析的字符，则报错
+        fprintf(stderr, "unexpected character: '%c'\n", *P);
+        return 1;
+    }  
+
+    // ret为 <jalr x0, x1, 0>别名指令， 用于返回子程序
+    printf("\tret\n");
+    
+    return 0;
+}
+
